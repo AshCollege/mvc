5c50daa78914fa0cdce024fcf14a4804f2515a64
oriel <ori120shay@gmail.com>
2019-05-20 19:59:59 +0300

    new dashboard controller

---
+++ src/main/java/com/dev/Controllers/DashboardController.java
@@ -0,0 +1,21 @@
+package com.dev.Controllers;
+
+import com.dev.Services.GeneralManager;
+import com.dev.Utils.ConfigUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Controller
+public class DashboardController {
+
+    @RequestMapping("/dasboard")
+    public String dashboard() {
+        return "tmpl_dashboard";
+    }
+
+}
