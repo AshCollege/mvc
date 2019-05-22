7c3440bc0702ad6c10240f70d1688d60067c5482
OritNagossa <orit605@gmail.com>
2019-05-22 20:28:43 +0200

    seller page connection to database pull seller data products table

---
+++ src/main/java/com/dev/Controllers/SellerController.java
@@ -0,0 +1,30 @@
+package com.dev.Controllers;
+
+import com.dev.Objects.Entities.Seller;
+import com.dev.Services.GeneralManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+@Controller
+public class SellerController {
+
+    @Autowired
+    private GeneralManager generalManager;
+
+    @RequestMapping("/seller/{oid}")
+    public String about(Model model, @PathVariable int oid) {
+        Seller seller = generalManager.loadObject(Seller.class, oid);
+        if (seller != null) {
+            model.addAttribute("seller", seller);
+        } else {
+            model.addAttribute("error", true);
+        }
+
+        return "tmpl_seller";
+    }
+}
---
+++ src/main/java/com/dev/Objects/Entities/Seller.java
@@ -0,0 +1,66 @@
+package com.dev.Objects.Entities;
+
+import com.dev.Objects.General.BaseEntity;
+
+import java.util.Date;
+
+public class Seller extends BaseEntity {
+    private String name;
+    private String lastName;
+    private Date dateOfBirth;
+    private int phone;
+    private String adress;
+    public Seller(){
+
+    }
+
+
+    public Seller(String name, String lastName, Date dateOfBirth, int phone, String adress) {
+        this.name = name;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.phone = phone;
+        this.adress = adress;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+}
